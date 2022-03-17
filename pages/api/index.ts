254d19a0f26e57c52ff01588f6bd8c67d85fa158
Omotolani <torhlarny15@gmail.com>
2022-03-17 13:31:12 +0100

    sign up and login implemented

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,5 +1,8 @@
-const hello = () => {
-  console.log("Hello World");
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+const hello = async (req: any, res: any) => {
+  const response = await prisma.user.findMany();
+  return res.status(200).send({ message: "successful", user: response });
 };
 
 export default hello;
